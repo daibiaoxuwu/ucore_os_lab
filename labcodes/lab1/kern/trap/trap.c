0d5d641f27c410df84e2480782c0e0d30a9c5946
daibiaoxuwu <dujl16@mails.tsinghua.edu.cn>
2019-03-22 18:31:12 +0800

    temp

--- labcodes/lab1/kern/trap/trap.c
+++ labcodes/lab1/kern/trap/trap.c
@@ -44,134 +44,149 @@ idt_init(void) {
       *     Can you see idt[256] in this file? Yes, it's IDT! you can use SETGATE macro to setup each item of IDT
       * (3) After setup the contents of IDT, you will let CPU know where is the IDT by using 'lidt' instruction.
       *     You don't know the meaning of this instruction? just google it! and check the libs/x86.h to know more.
-      *     Notice: the argument of lidt is idt_pd. try to find it!
-      */
+	  *     Notice: the argument of lidt is idt_pd. try to find it!
+	  */
+	extern uintptr_t __vectors[];
+	for (int i = 0; i < 256; ++ i) {
+		//		cprintf("vectors %d: 0x%08x\n", i, __vectors[i]);
+		if (i == T_SYSCALL || i == T_SWITCH_TOK) {
+			SETGATE(idt[i], 1, KERNEL_CS, __vectors[i], DPL_USER);
+		} else {
+			SETGATE(idt[i], 0, KERNEL_CS, __vectors[i], DPL_KERNEL);
+		}
+	}
+	lidt(&idt_pd);
+
 }
 
 static const char *
 trapname(int trapno) {
-    static const char * const excnames[] = {
-        "Divide error",
-        "Debug",
-        "Non-Maskable Interrupt",
-        "Breakpoint",
-        "Overflow",
-        "BOUND Range Exceeded",
-        "Invalid Opcode",
-        "Device Not Available",
-        "Double Fault",
-        "Coprocessor Segment Overrun",
-        "Invalid TSS",
-        "Segment Not Present",
-        "Stack Fault",
-        "General Protection",
-        "Page Fault",
-        "(unknown trap)",
-        "x87 FPU Floating-Point Error",
-        "Alignment Check",
-        "Machine-Check",
-        "SIMD Floating-Point Exception"
-    };
+	static const char * const excnames[] = {
+		"Divide error",
+		"Debug",
+		"Non-Maskable Interrupt",
+		"Breakpoint",
+		"Overflow",
+		"BOUND Range Exceeded",
+		"Invalid Opcode",
+		"Device Not Available",
+		"Double Fault",
+		"Coprocessor Segment Overrun",
+		"Invalid TSS",
+		"Segment Not Present",
+		"Stack Fault",
+		"General Protection",
+		"Page Fault",
+		"(unknown trap)",
+		"x87 FPU Floating-Point Error",
+		"Alignment Check",
+		"Machine-Check",
+		"SIMD Floating-Point Exception"
+	};
 
-    if (trapno < sizeof(excnames)/sizeof(const char * const)) {
-        return excnames[trapno];
-    }
-    if (trapno >= IRQ_OFFSET && trapno < IRQ_OFFSET + 16) {
-        return "Hardware Interrupt";
-    }
-    return "(unknown trap)";
+	if (trapno < sizeof(excnames)/sizeof(const char * const)) {
+		return excnames[trapno];
+	}
+	if (trapno >= IRQ_OFFSET && trapno < IRQ_OFFSET + 16) {
+		return "Hardware Interrupt";
+	}
+	return "(unknown trap)";
 }
 
 /* trap_in_kernel - test if trap happened in kernel */
 bool
 trap_in_kernel(struct trapframe *tf) {
-    return (tf->tf_cs == (uint16_t)KERNEL_CS);
+	return (tf->tf_cs == (uint16_t)KERNEL_CS);
 }
 
 static const char *IA32flags[] = {
-    "CF", NULL, "PF", NULL, "AF", NULL, "ZF", "SF",
-    "TF", "IF", "DF", "OF", NULL, NULL, "NT", NULL,
-    "RF", "VM", "AC", "VIF", "VIP", "ID", NULL, NULL,
+	"CF", NULL, "PF", NULL, "AF", NULL, "ZF", "SF",
+	"TF", "IF", "DF", "OF", NULL, NULL, "NT", NULL,
+	"RF", "VM", "AC", "VIF", "VIP", "ID", NULL, NULL,
 };
 
 void
 print_trapframe(struct trapframe *tf) {
-    cprintf("trapframe at %p\n", tf);
-    print_regs(&tf->tf_regs);
-    cprintf("  ds   0x----%04x\n", tf->tf_ds);
-    cprintf("  es   0x----%04x\n", tf->tf_es);
-    cprintf("  fs   0x----%04x\n", tf->tf_fs);
-    cprintf("  gs   0x----%04x\n", tf->tf_gs);
-    cprintf("  trap 0x%08x %s\n", tf->tf_trapno, trapname(tf->tf_trapno));
-    cprintf("  err  0x%08x\n", tf->tf_err);
-    cprintf("  eip  0x%08x\n", tf->tf_eip);
-    cprintf("  cs   0x----%04x\n", tf->tf_cs);
-    cprintf("  flag 0x%08x ", tf->tf_eflags);
+	cprintf("trapframe at %p\n", tf);
+	print_regs(&tf->tf_regs);
+	cprintf("  ds   0x----%04x\n", tf->tf_ds);
+	cprintf("  es   0x----%04x\n", tf->tf_es);
+	cprintf("  fs   0x----%04x\n", tf->tf_fs);
+	cprintf("  gs   0x----%04x\n", tf->tf_gs);
+	cprintf("  trap 0x%08x %s\n", tf->tf_trapno, trapname(tf->tf_trapno));
+	cprintf("  err  0x%08x\n", tf->tf_err);
+	cprintf("  eip  0x%08x\n", tf->tf_eip);
+	cprintf("  cs   0x----%04x\n", tf->tf_cs);
+	cprintf("  flag 0x%08x ", tf->tf_eflags);
 
-    int i, j;
-    for (i = 0, j = 1; i < sizeof(IA32flags) / sizeof(IA32flags[0]); i ++, j <<= 1) {
-        if ((tf->tf_eflags & j) && IA32flags[i] != NULL) {
-            cprintf("%s,", IA32flags[i]);
-        }
-    }
-    cprintf("IOPL=%d\n", (tf->tf_eflags & FL_IOPL_MASK) >> 12);
+	int i, j;
+	for (i = 0, j = 1; i < sizeof(IA32flags) / sizeof(IA32flags[0]); i ++, j <<= 1) {
+		if ((tf->tf_eflags & j) && IA32flags[i] != NULL) {
+			cprintf("%s,", IA32flags[i]);
+		}
+	}
+	cprintf("IOPL=%d\n", (tf->tf_eflags & FL_IOPL_MASK) >> 12);
 
-    if (!trap_in_kernel(tf)) {
-        cprintf("  esp  0x%08x\n", tf->tf_esp);
-        cprintf("  ss   0x----%04x\n", tf->tf_ss);
-    }
+	if (!trap_in_kernel(tf)) {
+		cprintf("  esp  0x%08x\n", tf->tf_esp);
+		cprintf("  ss   0x----%04x\n", tf->tf_ss);
+	}
 }
 
 void
 print_regs(struct pushregs *regs) {
-    cprintf("  edi  0x%08x\n", regs->reg_edi);
-    cprintf("  esi  0x%08x\n", regs->reg_esi);
-    cprintf("  ebp  0x%08x\n", regs->reg_ebp);
-    cprintf("  oesp 0x%08x\n", regs->reg_oesp);
-    cprintf("  ebx  0x%08x\n", regs->reg_ebx);
-    cprintf("  edx  0x%08x\n", regs->reg_edx);
-    cprintf("  ecx  0x%08x\n", regs->reg_ecx);
-    cprintf("  eax  0x%08x\n", regs->reg_eax);
+	cprintf("  edi  0x%08x\n", regs->reg_edi);
+	cprintf("  esi  0x%08x\n", regs->reg_esi);
+	cprintf("  ebp  0x%08x\n", regs->reg_ebp);
+	cprintf("  oesp 0x%08x\n", regs->reg_oesp);
+	cprintf("  ebx  0x%08x\n", regs->reg_ebx);
+	cprintf("  edx  0x%08x\n", regs->reg_edx);
+	cprintf("  ecx  0x%08x\n", regs->reg_ecx);
+	cprintf("  eax  0x%08x\n", regs->reg_eax);
 }
 
 /* trap_dispatch - dispatch based on what type of trap occurred */
 static void
 trap_dispatch(struct trapframe *tf) {
-    char c;
+	char c;
 
-    switch (tf->tf_trapno) {
-    case IRQ_OFFSET + IRQ_TIMER:
-        /* LAB1 YOUR CODE : STEP 3 */
-        /* handle the timer interrupt */
-        /* (1) After a timer interrupt, you should record this event using a global variable (increase it), such as ticks in kern/driver/clock.c
-         * (2) Every TICK_NUM cycle, you can print some info using a funciton, such as print_ticks().
-         * (3) Too Simple? Yes, I think so!
-         */
-        break;
-    case IRQ_OFFSET + IRQ_COM1:
-        c = cons_getc();
-        cprintf("serial [%03d] %c\n", c, c);
-        break;
-    case IRQ_OFFSET + IRQ_KBD:
-        c = cons_getc();
-        cprintf("kbd [%03d] %c\n", c, c);
-        break;
-    //LAB1 CHALLENGE 1 : YOUR CODE you should modify below codes.
-    case T_SWITCH_TOU:
-    case T_SWITCH_TOK:
-        panic("T_SWITCH_** ??\n");
-        break;
-    case IRQ_OFFSET + IRQ_IDE1:
-    case IRQ_OFFSET + IRQ_IDE2:
-        /* do nothing */
-        break;
-    default:
-        // in kernel, it must be a mistake
-        if ((tf->tf_cs & 3) == 0) {
-            print_trapframe(tf);
-            panic("unexpected trap in kernel.\n");
-        }
-    }
+	switch (tf->tf_trapno) {
+		case IRQ_OFFSET + IRQ_TIMER:
+			/* LAB1 YOUR CODE : STEP 3 */
+			/* handle the timer interrupt */
+			/* (1) After a timer interrupt, you should record this event using a global variable (increase it), such as ticks in kern/driver/clock.c
+			 * (2) Every TICK_NUM cycle, you can print some info using a funciton, such as print_ticks().
+			 * (3) Too Simple? Yes, I think so!
+			 */
+            if(++ticks==100){
+                print_ticks();
+                ticks=0;
+            }
+			break;
+		case IRQ_OFFSET + IRQ_COM1:
+			c = cons_getc();
+			cprintf("serial [%03d] %c\n", c, c);
+			break;
+		case IRQ_OFFSET + IRQ_KBD:
+			c = cons_getc();
+			cprintf("kbd [%03d] %c\n", c, c);
+			break;
+			//LAB1 CHALLENGE 1 : YOUR CODE you should modify below codes.
+		case T_SWITCH_TOU:
+		case T_SWITCH_TOK:
+			panic("T_SWITCH_** ??\n");
+			break;
+		case IRQ_OFFSET + IRQ_IDE1:
+		case IRQ_OFFSET + IRQ_IDE2:
+			/* do nothing */
+			break;
+		default:
+			// in kernel, it must be a mistake
+			if ((tf->tf_cs & 3) == 0) {
+				print_trapframe(tf);
+				panic("unexpected trap in kernel.\n");
+			}
+	}
 }
 
 /* *
@@ -181,7 +196,7 @@ trap_dispatch(struct trapframe *tf) {
  * */
 void
 trap(struct trapframe *tf) {
-    // dispatch based on what type of trap occurred
-    trap_dispatch(tf);
+	// dispatch based on what type of trap occurred
+	trap_dispatch(tf);
 }
 
